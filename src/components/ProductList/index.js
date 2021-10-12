97b872a4bfdc6ab8856a0034816452052ad19072
Roberto Rocha <rsrocha@usp.br>
2021-10-12 16:40:55 -0300

    feat: implementation of the beers list (main page)

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Grid, Typography } from "@material-ui/core";
 
 const ProductList = () => {
     const [beers, setBeers] = useState([]);
 
     const getListOfBeers = () => {
         axios
-            .get("https://api.punkapi.com/v2/beers")
+            .get("https://api.punkapi.com/v2/beers?per_page=80")
             .then((response) => {
                 console.log(response.data);
                 setBeers(response.data);
@@ -30,7 +31,61 @@ const ProductList = () => {
         getListOfBeers();
     }, []);
 
-    return beers.map((beer) => <div key={beer.id}>{beer.name}</div>);
+    return (
+        <Grid container justifyContent="center">
+            {beers.map((beer) => (
+                <Box key={beer.id} sx={{ width: 370 }}>
+                    <Grid item display="flex" mt={2}>
+                        <Card sx={{ backgroundColor: "rgb(204 149 192 / 0.3)", display: "flex", flexDirection: "column", justifyContent: "space-between", height: 675, width: 365 }}>
+                            <CardHeader
+                                disableTypography
+                                avatar={
+                                    <Avatar sx={{ bgcolor: "blueviolet", fontWeight: 600, width: 56, height: 56 }}>
+                                        {beer.name.substr(0, 1)}
+                                    </Avatar>
+                                }
+                                title={
+                                    <Typography
+                                        sx={{ fontSize: 27 }}
+                                    >
+                                        {beer.name}
+                                    </Typography>
+                                }
+                                subheader={
+                                    <Typography fontSize={13}>
+                                        {beer.tagline}
+                                    </Typography>
+                                }
+                            />
+                            <Box sx={{ width: 75 }}>
+                                <CardMedia
+                                    component="img"
+                                    image={beer.image_url}
+                                    alt={beer.name}
+                                    sx={{ ml: 17 }}
+                                />
+                            </Box>
+                            <CardContent>
+                                <Typography sx={{ mb: 1.5 }} color="blueviolet">
+                                    {`Since ${beer.first_brewed}`}
+                                </Typography>
+                                <Typography variant="h6" color="blueviolet">
+                                    Description:
+                                </Typography>
+                                <Typography variant="body2" sx={{ fontSize: 11 }}>
+                                    {beer.description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                </Box>
+            ))}
+        </Grid>
+    );
 };
 
 export default ProductList;
